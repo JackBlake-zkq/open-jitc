17d5101a85d2ebb03b54a4a40b33040f76ffb06c
Jack Blake <jhblake2@wisc.edu>
2025-03-25 16:26:39 -0400

    add Oobleck stuff

---
+++ setup_machine.sh
@@ -0,0 +1,6 @@
+pip install oobleck
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/ /"
+sudo apt-get install -y datacenter-gpu-manager
+sudo systemctl --now enable nvidia-dcgm
