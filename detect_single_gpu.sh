78a2de6e11b6d3936b1e77fb2ac95040b382ec38
Jack Blake <jhblake2@wisc.edu>
2025-04-11 10:49:22 -0500

    fix group id get

--- detect_single_gpu.sh
+++ detect_single_gpu.sh
@@ -1,6 +1,5 @@
 dcgmi policy --set 0,0 -e -p -n -x
-s=$(dcgmi group -c SingleGPU | tr -dc '0-9')
-group_id=${s: -1}
+group_id=$(dcgmi group -c SingleGPU | tr -dc '0-9')
 dcgmi group -g $group_id -a $0
 dcgmi policy --reg -g $group_id &
 while true; do sleep 1 && nvidia-smi --id=$0 | grep -E "Segmentation fault|No devices were found|Unable to determine the device handle for"; done &
