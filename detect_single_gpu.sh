04014c210f919f8ec4e4ac8085fcba2430c2851b
Jack Blake <jhblake2@wisc.edu>
2025-04-11 10:37:00 -0500

    add filter on svidia-smi

--- detect_single_gpu.sh
+++ detect_single_gpu.sh
@@ -2,5 +2,5 @@ s=$(dcgmi group -c SingleGPU | tr -dc '0-9')
 group_id=${s: -1}
 dcgmi group -g $group_id -a $0
 dcgmi policy --reg -g $group_id &
-while true; do sleep 1 && nvidia-smi --id=0; done &
+while true; do sleep 1 && nvidia-smi | grep -E "Segmentation fault|No devices were found|Unable to determine the device handle for" --id=0; done &
 sleep infinity
