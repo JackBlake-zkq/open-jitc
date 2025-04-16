32344b9fa3f42158c62edd21cb5f19042e073a47
Jack Blake <jhblake2@wisc.edu>
2025-04-16 16:09:53 -0500

    try other log

--- TransparentJITC/nccl_and_cuda_logger.cpp
+++ TransparentJITC/nccl_and_cuda_logger.cpp
@@ -130,5 +130,10 @@ ncclResult_t ncclAllReduce(const void* sendbuff, void* recvbuff, size_t count,
         fprintf(log_file, "[NCCL] ncclAllReduce result: %d\n", result);
         fflush(log_file);
     }
+    char * error = ncclGetLastError(comm);
+    if (error) {
+        fprintf(log_file, "[NCCL] ncclAllReduce error: %s\n", error);
+        fflush(log_file);
+    }
     return result;
 }
